c88ec922686a4633cfcc6a6b8a6794e26f7d8450
Cayce <cayce@shawnhouse.com>
2013-02-10 22:23:06 -0500

    @alexhairyman's feature request
    Made it to where if the hours / minutes / seconds is less than 10 then
    it adds a zero to the front.

--- clock/clock.js
+++ clock/clock.js
@@ -9,7 +9,19 @@ var curr_seconds = d.getSeconds();
 if (curr_hour > 12) {
 	var hourModified = curr_hour - 12;
 } else {
-	var hourModified = curr_hour;
+	if (curr_hour < 10) {
+		var hourModified = "0" + curr_hour;
+	} else {
+		var hourModified = curr_hour;
+	}
+}
+
+if (curr_min < 10) {
+	var curr_min = "0" + curr_min;
+}
+
+if (curr_seconds < 10) {
+	var curr_seconds = "0" + curr_seconds;
 }
 
 var myTime = (hourModified + " : " + curr_min + " : " + curr_seconds);
