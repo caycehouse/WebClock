7199faae9e1e3b260d1a929064a7c7b94da021ad
Cayce <cayce@shawnhouse.com>
2013-02-11 16:25:26 -0500

    Various bug fixes, and changes
    - missing closing div tag on index.php
    - added 0's the front of hr/min/sec if the number is less than 10

--- clock/clock.js
+++ clock/clock.js
@@ -1,33 +1,37 @@
 function displayDate() {
 
-var d = new Date();
+	var d = new Date();
 
-var curr_hour = d.getHours();
-var curr_min = d.getMinutes();
-var curr_seconds = d.getSeconds();
+	var curr_hour = d.getHours();
+	var curr_min = d.getMinutes();
+	var curr_seconds = d.getSeconds();
 
-if (curr_hour > 12) {
-	var hourModified = curr_hour - 12;
-} else {
-	if (curr_hour < 10) {
-		var hourModified = "0" + curr_hour;
+	if (curr_hour > 12) {
+		var hourModified = curr_hour - 12;
+		if (hourModified < 10) {
+			var hourModified = "0" + hourModified;
+		}
 	} else {
-		var hourModified = curr_hour;
+		if (curr_hour < 10) {
+			var hourModified = "0" + curr_hour;
+		} else {
+			var hourModified = curr_hour;
+		}
 	}
-}
 
-if (curr_min < 10) {
-	var curr_min = "0" + curr_min;
-}
+	if (curr_min < 10) {
+		var curr_min = "0" + curr_min;
+	}
 
-if (curr_seconds < 10) {
-	var curr_seconds = "0" + curr_seconds;
-}
+	if (curr_seconds < 10) {
+		var curr_seconds = "0" + curr_seconds;
+	}
 
-var myTime = (hourModified + " : " + curr_min + " : " + curr_seconds);
+	var myTime = (hourModified + " : " + curr_min + " : " + curr_seconds);
+
 	document.getElementById('time').innerHTML = myTime;
 }
 
 window.setInterval(function() {
-displayDate();
+	displayDate();
 }, 1000);
--- clock/index.php
+++ clock/index.php
@@ -3,9 +3,12 @@
 <table id="table">
 <th>Your Current Time:</th>
 <tr>
-<td bgcolor="#FFFFFF"><div id="time"></td>
+<td bgcolor="#FFFFFF"><div id="time"></div></td>
 </tr>
 </table>
+
+<br />
+<div id="copy">&copy; 2013 Cayce House<div>
 
 <script type="text/javascript" src="clock.js"></script>
 
