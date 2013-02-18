c6ba2d1749ae457997cc2c3730ccdfe2f6b4a3a5
Cayce <cayce@datafarms.com>
2013-02-17 23:33:57 -0500

    Themes!
    - added themes!
    - *new* christmas theme
    - *new* halloween theme
    
    More To Come!

--- clock/inc/color.js
+++ clock/inc/color.js
@@ -1,52 +1,92 @@
 <!--
 
-function flipThose(color) {
+var flipThat = new Array();
+flipThat[0] = document.getElementById("title");
+flipThat[1] = document.getElementById("copy");
+flipThat[2] = document.getElementById("colorLinkRed");
+flipThat[3] = document.getElementById("colorLinkGreen");
+flipThat[4] = document.getElementById("colorLinkBlue");
+flipThat[5] = document.getElementById("colorLinkWhite");
+flipThat[6] = document.getElementById("colorLinkBlack");
+flipThat[7] = document.getElementById("colorLinkT1");
+flipThat[8] = document.getElementById("colorLinkT2");
+flipThat[100] = document.getElementById("time");
+flipThat[101] = document.getElementById("clockBody");
 
-	var flipThat = new Array();
+function resetTheme() {
+var body = document.getElementsByTagName('body')[0];
+body.style.backgroundImage = '';
+}
 
-	flipThat[0] = document.getElementById("title");
-	flipThat[1] = document.getElementById("copy");
-	flipThat[2] = document.getElementById("colorLinkRed");
-	flipThat[3] = document.getElementById("colorLinkGreen");
-	flipThat[4] = document.getElementById("colorLinkBlue");
-	flipThat[5] = document.getElementById("colorLinkWhite");
-	flipThat[6] = document.getElementById("colorLinkBlack");
-	flipThat[7] = document.getElementById("time");
-	flipThat[8] = document.getElementById("flipThatBody");
-
-	for(x=0; x<7; x++) {
-		flipThat[x].style.color = color;
-	}
+function changeTheme(whichTheme) {
+	resetTheme();
+	var defaultThemes = new Array();
+	defaultThemes[0] = "halloween";
+	defaultThemes[1] = "christmas";
 	
-	if (color == "#FFFFFF") {
-		flipThat[8].style.backgroundColor = "#FFFFFF";
-		flipThat[7].style.color = "#000000";
-	} else if (color == "#000000") {
-		flipThat[8].style.backgroundColor = "#000000";
-		flipThat[7].style.color = "#FFFFFF";
+	var theme = defaultThemes[whichTheme];
+	if (theme == "halloween") {
+		var body = document.getElementsByTagName('body')[0];
+		body.style.backgroundImage = 'url(inc/halloweenBG.jpg)';
+		changeDefaults("black");
+
+		for(x=0; x<9; x++) {
+			flipThat[x].style.color = "#FFFFFF";
+		}
+	}
+	if (theme == "christmas") {
+		var body = document.getElementsByTagName('body')[0];
+		body.style.backgroundImage = 'url(inc/christmasBG.jpg)';
+		changeDefaults("black");
+	
+		for(x=0; x<9; x++) {
+			flipThat[x].style.color = "#000000";
+		}
+	}
+}
+
+function changeDefaults(defaultsColor){
+	var color = defaultsColor;
+
+	if (color == "white") {
+		for(x=0; x<9; x++) {
+			flipThat[x].style.color = "#FFFFFF";
+		}
+		flipThat[100].style.color = "#000000";
+		flipThat[101].style.backgroundColor = "#FFFFFF";
 	}
 
+	if (color == "black") {
+		for(x=0; x<9; x++) {
+			flipThat[x].style.color = "#000000";
+		}
+		flipThat[100].style.color = "#FFFFFF";
+		flipThat[101].style.backgroundColor = "#000000";
+	}
 }
 
 function changeBG(whichColor){
-	
 	var colorChanger = new Array();
+	colorChanger[0] = '#800000'; //red
+	colorChanger[1] = '#008000'; //green
+	colorChanger[2] = '#0000A0'; //blue
+	colorChanger[3] = '#FFFFFF'; //white
+	colorChanger[4] = '#000000'; //black
 
-	colorChanger[0] = '#800000';
-	colorChanger[1] = '#008000';
-	colorChanger[2] = '#0000A0';
-	colorChanger[3] = '#FFFFFF';
-	colorChanger[4] = '#000000';
-
-	document.bgColor = colorChanger[whichColor];
-	color = colorChanger[whichColor];
+	document.bgColor = colorChanger[whichColor]; //change background color to color chosen
+	var color = colorChanger[whichColor]; //set var color equal to the color chosen
 
 	if (color == "#FFFFFF") {
-		flipThose("#000000");
+		resetTheme();
+		changeDefaults("black");
 	} else if(color == "#000000") {
-		flipThose("#FFFFFF");
+		resetTheme();
+		changeDefaults("white");
+	} else if(color == "#FFA500") {
+		changeTheme("halloween");
 	} else {
-		flipThose("#FFFFFF");
+		resetTheme();
+		changeDefaults("white");
 	}
 
 }
